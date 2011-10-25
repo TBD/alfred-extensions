f4ec988f8212d07179bf1fcec05e7f2f0da1f6cd
TBD <tbd_fr@yahoo.fr>
2011-10-25 16:29:29 +0300

    bugfix PlayPause to work with several windows open

--- build.rb
+++
@@ -1,7 +0,0 @@
-Dir['*'].each do |dir|
-  next if dir == '_downloads'
-  next if not File.directory?(dir)
-  `zip -vr "#{dir}.zip" "#{dir}" -x "*.DS_Store"`
-  `mv "#{dir}.zip" ./_downloads/"#{dir}.alfredextension"`
-  puts "#{dir}.alfredextension created" 
-end
